9573267a9574eeeeb9096ee05d340320c56d8ae3
virkumar <virkumar@microsoft.com>
2022-01-15 06:02:36 +0530

    test intellij

--- src/main/java/MercFirstDemo.java
+++ src/main/java/MercFirstDemo.java
@@ -15,7 +15,7 @@ public class MercFirstDemo extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.getWriter().print("MercFirstDemo Modified for Java!\n");
+		resp.getWriter().print("MercFirstDemo Modified IntelliJ for Java!\n");
 	}
 
 	public static void main(String[] args) throws Exception {
